9880eccb2f03350e6e68a7d76fa39bb92945b624
Tunahan <karakoktunahan78@gmail.com>
2022-03-17 18:15:32 +0300

    Chaaracter controller upgarted

--- Assets/Scripts/Bullet/BulletCollisionController.cs
+++ Assets/Scripts/Bullet/BulletCollisionController.cs
@@ -7,11 +7,21 @@ public class BulletCollisionController : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Character"))
         {
-            
+            other.GetComponent<CharacterController>().GetDamage(10);
+            gameObject.SetActive(false);
         }
         else if (other.CompareTag("Obstacle"))
         {
             gameObject.SetActive(false);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            gameObject.SetActive(false);
+            other.gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/Characters/CharacterController.cs
+++ Assets/Scripts/Characters/CharacterController.cs
@@ -1,28 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public enum CharacterType{
-        player,
-        enemy
-    }
+
+public enum CharacterType
+{
+    player,
+    enemy
+}
 public class CharacterController : MonoBehaviour
 {
     public CharacterType characterType;
     public Transform firePoint;
     public float force;
+    public Image healthBar, damageBar;
+    public const float Max_Health = 100;
+    public const float Min_Health = 0;
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Backspace) && characterType == CharacterType.player)
+    private float _health;
+
+    private void Start()
+    {
+        _health = Max_Health;
+        UpdateHealthBar();
+        if (characterType == CharacterType.enemy)
         {
-            Fire(BulletPoolManager.Instance.GetBullet());
+            StartCoroutine(StartFireForEnemyType());
         }
     }
 
-    public  void Fire(GameObject bullet)
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && characterType == CharacterType.player)
+        {
+            Fire(BulletPoolManager.Instance.GetBullet());
+        }
+
+
+    }
+
+    #region HEALTH BAR FUNCTIONS
+    
+    /// <summary>
+    /// if character take damage we calling this func and update helath bar on Player
+    /// </summary>
+    public void UpdateHealthBar()
+    {
+        healthBar.fillAmount = _health / 100;
+        StartCoroutine(UpdateDamageBar());
+    }
+
+    IEnumerator UpdateDamageBar()
+    {
+        yield return new WaitForSeconds(.1f);
+        damageBar.fillAmount = _health / 100;
+    }
+
+    #endregion
+
+    /// <summary>
+    /// fire function 
+    /// </summary>
+    /// <param name="bullet"> get bullet in bullet pool manager in queue </param>
+    public void Fire(GameObject bullet)
     {
         bullet.transform.position = firePoint.position;
         bullet.SetActive(true);
         bullet.GetComponent<Rigidbody>().velocity = firePoint.TransformDirection(Vector3.forward) * force;
+    }
+
+    /// <summary>
+    /// if collise a bullet this func. calling both character type 
+    /// </summary>
+    /// <param name="damage"> damage value </param>
+    public void GetDamage(float damage)
+    {
+        _health -= damage;
+        UpdateHealthBar();
+        Debug.Log(transform.name + _health);
+        if (_health <= Min_Health)
+        {
+            // game over 
+        }
+    }
+
+
+    IEnumerator StartFireForEnemyType()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.75f);
+            Fire(BulletPoolManager.Instance.GetBullet());
+        }
     }
 }
